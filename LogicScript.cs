7f10ae2d0c37294b265c732074416b69960c1748
Ashish Pant <104633956+ashishpant31@users.noreply.github.com>
2023-01-16 23:13:57 +0530

    Add files via upload

---
+++ LogicScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LogicScript : MonoBehaviour
+{
+    public int playerScore;
+    public Text scoreText;
+    public GameObject gameOverScreen;
+    public AudioSource clingSFX;
+
+    [ContextMenu("Increase Score")]
+    public void addScore(int scoreToAdd)
+    {
+        playerScore += scoreToAdd;
+        scoreText.text = playerScore.ToString();
+        clingSFX.Play();
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
+    }
+}
